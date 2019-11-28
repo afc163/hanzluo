47e84608df399c4d27f52bb251955649f36ec6f8
Hanz Luo <lhz0516@gmail.com>
2019-11-28 14:48:08 -0800

    use bridge network

--- README.md
+++ README.md
@@ -18,5 +18,5 @@ $ ./deploy.sh
 
 Run Mongo Docker
 ```
-$ sudo docker run --name mongodb --net hanz-net -p 27017:27017 -dit mongo:4.1.13-bionic mongod --bind_ip_all
+$ sudo docker run --name mongodb --restart always --net bridge -p 27017:27017 -dit mongo:3.6.15-xenial mongod --bind_ip_all
 ```
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -x
 
 sudo docker stop hanzluo
-sudo docker rm hanzluo
+sudo docker rm -f hanzluo
 sudo docker image rm hanzluo:latest
 sudo docker build -t hanzluo .
-sudo docker run --name hanzluo --net hanz-net -p 3000:3000 --restart always -dit hanzluo:latest
+sudo docker run --name hanzluo --net bridge -p 3000:3000 --restart always -dit hanzluo:latest
