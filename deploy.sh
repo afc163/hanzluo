4d24fcf8dd62c869b1cb26410c6070d87a9710b8
Hanz Luo <lhz0516@gmail.com>
2019-11-29 07:40:47 +0000

    add docker-compose and update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -x
 sudo docker stop hanzluo
 sudo docker rm -f hanzluo
 sudo docker image rm hanzluo:latest
-sudo docker build -t hanzluo .
-sudo docker run --name hanzluo --net bridge -p 3000:3000 --restart always -dit hanzluo:latest
+sudo docker-compose up -d
+
